d0a2e8a8f800597188c41ddf511b49f9ea703550
Jeon Seung Hwan <fabronjeon@naver.com>
2025-12-18 10:14:41 +0900

    midterm.chap5: inpre + insertLeft

--- src/midterm/chap5/ThreadedBinaryTree.java
+++ src/midterm/chap5/ThreadedBinaryTree.java
@@ -15,6 +15,14 @@ public class ThreadedBinaryTree<E> {
 		return temp;
 	}	
 	
+	private ThreadedTreeNode<E> inpre(ThreadedTreeNode<E> p) {
+		ThreadedTreeNode<E> temp = p.getLeft();
+		if (p.lthread == false) 
+			while (temp.rthread == false)
+				temp = temp.getRight();
+		return temp;
+	}
+	
 	public List<ThreadedTreeNode<E>> inorder() {
 		List<ThreadedTreeNode<E>> result = new ArrayList<>();
 		ThreadedTreeNode<E> p = root;
@@ -40,6 +48,22 @@ public class ThreadedBinaryTree<E> {
 		if (child.rthread == false) {
 			ThreadedTreeNode<E> temp = insucc(child);
 			temp.left = child;
+		}
+	}
+	
+	public void insertLeft(ThreadedTreeNode<E> pParent, ThreadedTreeNode<E> pChild) {
+		ThreadedTreeNode<E> parent = pParent;
+		ThreadedTreeNode<E> child = pChild;
+		
+		child.left = parent.left;
+		child.lthread = parent.lthread;
+		child.right = parent;
+		child.rthread = true;
+		parent.left = child;
+		parent.lthread = false;
+		if (child.lthread == false) {
+			ThreadedTreeNode<E> temp = inpre(child);
+			temp.right = child;
 		}
 	}
 }

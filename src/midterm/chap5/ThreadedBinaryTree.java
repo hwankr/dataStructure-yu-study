6425b8543e59ec55894826c73d31551bd8bd01b9
Jeon Seung Hwan <fabronjeon@naver.com>
2025-12-18 09:28:08 +0900

    midterm.chap5 : Threaded inorder p.49

--- src/midterm/chap5/ThreadedBinaryTree.java
+++ src/midterm/chap5/ThreadedBinaryTree.java
@@ -1,21 +1,29 @@
 package midterm.chap5;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThreadedBinaryTree<E> {
-//	private ThreadedTreeNode<E> insucc(ThreadedTreeNode<E> p) {
-//		ThreadedTreeNode<E> temp = p.getRight();
-//		if (p.rthread == false) 
-//			while (temp.lthread == false)
-//				temp = temp.getLeft();
-//		return temp;
-//	}
+	ThreadedTreeNode<E> root;
 	
 	private ThreadedTreeNode<E> insucc(ThreadedTreeNode<E> p) {
 		ThreadedTreeNode<E> temp = p.getRight();
-		
 		if (p.rthread == false) {
 			while (temp.lthread == false)
 				temp = temp.getLeft();
 		}
 		return temp;
+	}	
+	
+	public List<ThreadedTreeNode<E>> inorder() {
+		List<ThreadedTreeNode<E>> result = new ArrayList<>();
+		ThreadedTreeNode<E> p = root;
+		
+		while (true) {
+			p = insucc(p);
+			if (p == root) break;
+			result.add(p);
+		}
+		return result;
 	}
 }

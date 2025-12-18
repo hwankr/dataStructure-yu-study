d35e741acf9f75cbd05f77e1588e716ad2ab96f2
Jeon Seung Hwan <fabronjeon@naver.com>
2025-12-18 13:53:53 +0900

    midterm.chap5: weightedUnion

--- src/midterm/chap5/DisjointSet.java
+++ src/midterm/chap5/DisjointSet.java
@@ -20,4 +20,20 @@ public class DisjointSet {
     	int pj = find(j);
     	parent[pi] = pj;
     }
+
+    public void weightedUnion(int i, int j) {
+    	int pi = find(i);
+    	int pj = find(j);
+    	if (pi == pj) return;
+    	
+    	int temp = parent[i] + parent[j];
+    	if (parent[i] > parent[j]) {
+    		parent[i] = j;
+    		parent[j] = temp;
+    	}
+    	else {
+    		parent[j] = i;
+    		parent[i] = temp;
+    	}
+    }
 }
